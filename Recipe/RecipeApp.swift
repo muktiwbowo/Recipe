9a85d3ee4618ebe2d388095f68c582ffb46ae5c1
Mukti Wibowo <muktiwbowo@gmail.com>
2021-12-23 13:34:16 +0700

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Mukti Wibowo on 22/12/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
